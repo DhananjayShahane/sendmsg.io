0400d76745e7aef53ae634f7e3576e1baab589ea
Dhananjay Shahane <143813959+DhananjayShahane@users.noreply.github.com>
2023-10-13 15:53:15 +0530

    update

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,32 @@
 $(document).ready(() => {
+  $("#form").validate({
+    roles:{
+      number_input :"required",
+    },
+    messages:{
+      number_input : "Mobile number required",
+    },
+    errorPlacement : (error,element)=>{
+      if(element.attr('type') === 'number'){
+        error.appendTo("#error_wrapper")
+      }else{
+        error.insertAfter(element)
+      }
+    }
+  });
 
  $("#submit").click(()=>{
    let numberArray = [];
    
    let whatsapp_numbers  =  $("#number_input").val();
-
-   if(whatsapp_numbers !==''){
+   
+   if($("#form").validate()) {
       numberArray.push(whatsapp_numbers);
       localStorage.setItem("saved_wh_numbers",JSON.stringify(numberArray));
       window.location = `http://wa.me/${whatsapp_numbers}`;
       $("#number_input").val('');
-   }else{
-    alert("number is required");
    }
-    
-
+   
  }); 
  
  var saved_numberArray = JSON.parse(localStorage.getItem("saved_wh_numbers"));
